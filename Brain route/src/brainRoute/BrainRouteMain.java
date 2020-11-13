78ae9b7833646b48b99aa25c17b4a685226bf5a2
ADMIN <ADMIN@LAPTOP-JI2QAD8N.home-life.hub>
2020-11-13 13:22:52 +0100

    double enter fixed

--- Brain route/src/brainRoute/BrainRouteMain.java
+++ Brain route/src/brainRoute/BrainRouteMain.java
@@ -7,51 +7,48 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BrainRouteMain {
-
+	
+	static int Lvl = 1;
+	
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
-		CLIn cleaner = new CLIn();
-		int n = 4; // number of desired levels
-
+		
 		System.out.print("Press enter to start\n");
 		s.nextLine();
-		for (int i = 1; i <= n; i++) {
-			System.out.print("Level " + i + " \n");
-			oneLevel(3 * i - i, s, cleaner); // formula that generates levels
-		}
+		
+		oneLevel(3, s);
+		oneLevel(5, s);
+		oneLevel(8, s);
+		
 		System.out.print("Well done, you are a brain route genius!");
 		s.close();
 
 	}
 
-	public static void oneLevel(int length, Scanner s, CLIn cleaner) {
+	public static void oneLevel(int length, Scanner s) {
 
 		List<String> route = generateRoute(length);
 		String ans;
-
+		
+		System.out.println("Level" + Lvl + "\n");
+		
 		for (int j = 0; j < route.size(); j++) {
-			if (s.nextLine() != null) {
-				cleaner.clearCLI(); // terminal is cleared before printing next step
-				System.out.println(route.get(j));
-			}
+			System.out.println(route.get(j));	
 			ans = s.nextLine().toString();
 			if (j == route.size() - 1) {
-				if (ans.compareTo("s") == 0)
-					System.out.print("Good job, next level\n");
+				if (ans.compareTo("s") == 0) {
+					System.out.print("Good job, next level\n\n");
+					Lvl++;
+				}
 				else {
-					System.out.print("You are mistaken, try again\n");
-					oneLevel(length, s, cleaner);
+					System.out.print("You are mistaken, try again\n\n");
+					oneLevel(length, s);
 				}
 			} else {
 				if (ans.compareTo("s") == 0) {
-					/*
-					 * If user presses s before it should notify him then let him retry the level.
-					 * Once he completes the level return to the main, as user should not be
-					 * prompted with the remaining steps of the level he failed earlier.
-					 */
 					System.out.print("Too early, try again\n");
-					oneLevel(length, s, cleaner);
+					oneLevel(length, s);
 					return;
 				}
 			}
